b5ba3ecd06e19c10f40247e7d9c46e36d09f755d
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2023-07-24 11:50:46 -0400

    Add Resources Components

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,6 +11,11 @@ const Navbar = () => {
         <NavLink to='/login'>Login</NavLink>
         <NavLink to='/signup'>Signup</NavLink>
         <NavLink to='/about'>About</NavLink>
+        <NavLink to='/collections'>My Collections</NavLink>
+        <NavLink to='/profile'>Profile</NavLink>
+        <NavLink to='/logout'>Logout</NavLink>
+        <NavLink to='/looks'>My Looks</NavLink>
+        <NavLink to='/pieces'>My Pieces</NavLink>
       </nav>
     </>
   );
